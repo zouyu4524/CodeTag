b4e0438cf5745cb9c185e2f780a879fad2ef22f2
zouyu4524 <920789718@qq.com>
2017-06-21 17:13:56 +0800

    Change Indent from tab to 4 spaces

--- addTag.js
+++ addTag.js
@@ -1,30 +1,30 @@
 // trancode "<" and ">" in code snippet into html coding
 function transCode(text) {
-	var newStr;
-	newStr = text.replace(/</g, "&lt;");
-	newStr = newStr.replace(/>/g, "&gt;");
-	return newStr;
+    var newStr;
+    newStr = text.replace(/</g, "&lt;");
+    newStr = newStr.replace(/>/g, "&gt;");
+    return newStr;
 }
 
 // register listener on message, fired when a sendMessage called in background
 // in this function, selection is recognized and replaced with tag added context
 // different tags are determined by the message sent from background
 chrome.extension.onMessage.addListener(function (message, sender, callback) {
-	// get selection
-	var sel = window.getSelection();
-	var codeWithTag;
+    // get selection
+    var sel = window.getSelection();
+    var codeWithTag;
 
-	if (message.codetag == "block") {
-		codeWithTag = "<pre><code>" + transCode(sel.toString()) + "</code></pre>";
-	}
-	if (message.codetag === "inline") {
-		codeWithTag = "<code>" + transCode(sel.toString()) + "</code>";
-	}
-	var elem = document.activeElement;
-	var start = elem.selectionStart;
-	var end = elem.selectionEnd;
-	elem.value = elem.value.slice(0, start) + codeWithTag + elem.value.substr(end);
-	// Set cursor after selected text
-	elem.selectionStart = start + codeWithTag.length;
-	elem.selectionEnd = elem.selectionStart;
+    if (message.codetag == "block") {
+        codeWithTag = "<pre><code>" + transCode(sel.toString()) + "</code></pre>";
+    }
+    if (message.codetag === "inline") {
+        codeWithTag = "<code>" + transCode(sel.toString()) + "</code>";
+    }
+    var elem = document.activeElement;
+    var start = elem.selectionStart;
+    var end = elem.selectionEnd;
+    elem.value = elem.value.slice(0, start) + codeWithTag + elem.value.substr(end);
+    // Set cursor after selected text
+    elem.selectionStart = start + codeWithTag.length;
+    elem.selectionEnd = elem.selectionStart;
 });
--- codetag.js
+++ codetag.js
@@ -1,45 +1,45 @@
 // parent menu
 var parent = chrome.contextMenus.create({
-		"title": "Code Tag",
-		"contexts": ["selection"]
-	});
+        "title": "Code Tag",
+        "contexts": ["selection"]
+    });
 
 // sub-menu for block style
 chrome.contextMenus.create({
-	"title": "Block",
-	"parentId": parent,
-	"contexts": ["selection"],
-	"onclick": function (info, tab) {
-		if (info.editable) {
-			chrome.tabs.query({
-				"active": true,
-				"currentWindow": true
-			}, function (tabs) {
-				chrome.tabs.sendMessage(tabs[0].id, {
-					// codetag message, indicating block style
-					"codetag": "block"
-				});
-			});
-		}
-	}
+    "title": "Block",
+    "parentId": parent,
+    "contexts": ["selection"],
+    "onclick": function (info, tab) {
+        if (info.editable) {
+            chrome.tabs.query({
+                "active": true,
+                "currentWindow": true
+            }, function (tabs) {
+                chrome.tabs.sendMessage(tabs[0].id, {
+                    // codetag message, indicating block style
+                    "codetag": "block"
+                });
+            });
+        }
+    }
 });
 
 // sub-menu for inline style
 chrome.contextMenus.create({
-	"title": "Inline",
-	"parentId": parent,
-	"contexts": ["selection"],
-	"onclick": function (info, tab) {
-		if (info.editable) {
-			chrome.tabs.query({
-				"active": true,
-				"currentWindow": true
-			}, function (tabs) {
-				chrome.tabs.sendMessage(tabs[0].id, {
-					// codetag message, indicating inline style
-					"codetag": "inline"
-				});
-			});
-		}
-	}
+    "title": "Inline",
+    "parentId": parent,
+    "contexts": ["selection"],
+    "onclick": function (info, tab) {
+        if (info.editable) {
+            chrome.tabs.query({
+                "active": true,
+                "currentWindow": true
+            }, function (tabs) {
+                chrome.tabs.sendMessage(tabs[0].id, {
+                    // codetag message, indicating inline style
+                    "codetag": "inline"
+                });
+            });
+        }
+    }
 });
