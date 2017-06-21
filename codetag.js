41eb86e0e3d9e758869b8b8cdef68069135f65e5
zouyu4524 <920789718@qq.com>
2017-06-21 17:08:48 +0800

    Update version 0.2

--- codetag.js
+++ codetag.js
@@ -1,48 +1,45 @@
-function copyToClipboard(text) {
-	// window.prompt("Copy to clipboard: Ctrl+C, Enter", text);
-	copy(text);
-	//paste();
-}
-
+// parent menu
 var parent = chrome.contextMenus.create({
 		"title": "Code Tag",
 		"contexts": ["selection"]
 	});
 
+// sub-menu for block style
 chrome.contextMenus.create({
-		"title": "Block",
-		"parentId": parent,
-		"contexts": ["selection"],
-		"onclick": function(info, tab) {
-			if (info.editable){
-					copyToClipboard("<pre><code>" + info.selectionText + "</pre></code>");
-			}
+	"title": "Block",
+	"parentId": parent,
+	"contexts": ["selection"],
+	"onclick": function (info, tab) {
+		if (info.editable) {
+			chrome.tabs.query({
+				"active": true,
+				"currentWindow": true
+			}, function (tabs) {
+				chrome.tabs.sendMessage(tabs[0].id, {
+					// codetag message, indicating block style
+					"codetag": "block"
+				});
+			});
 		}
+	}
 });
 
+// sub-menu for inline style
 chrome.contextMenus.create({
-		"title": "Inline",
-		"parentId": parent,
-		"contexts": ["selection"],
-		"onclick": function(info, tab) {
-			if (info.editable){
-					copyToClipboard("<code>" + info.selectionText + "</code>");
-			}
+	"title": "Inline",
+	"parentId": parent,
+	"contexts": ["selection"],
+	"onclick": function (info, tab) {
+		if (info.editable) {
+			chrome.tabs.query({
+				"active": true,
+				"currentWindow": true
+			}, function (tabs) {
+				chrome.tabs.sendMessage(tabs[0].id, {
+					// codetag message, indicating inline style
+					"codetag": "inline"
+				});
+			});
 		}
+	}
 });
-
-function copy(str) {
-    var sandbox = $('#sandbox').val(str).select();
-    document.execCommand('copy');
-    sandbox.val('');
-}
-
-function paste() {
-    var result = '',
-        sandbox = $('#sandbox').val('').select();
-    if (document.execCommand('paste')) {
-        result = $("#sandbox").val();
-    }
-    sandbox.val('');
-    return result;
-}
